703ad69032eab237cb689b4b5b96ff545be29603
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-05-15 19:29:42 +0600

    Navbar, Footer & Banner Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Navbar = () => {
     return (
-        <div>
+        <nav className="flex justify-between items-center p-5 bg-white">
+            <ul className="flex justify-between items-center gap-3">
+                <li><Link href={"/"}>Home</Link></li>
+                <li><Link href={"/destinations"}>Destinations</Link></li>
+                <li><Link href={"/my-bookings"}>My Bookings</Link></li>
+                <li><Link href={"/admin"}>Admin</Link></li>
+            </ul>
+
+            <div>
+                <Image
+                src={"/assets/Wanderlast.png"}
+                alt="Wanderlast"
+                width={150}
+                height={150}
+                ></Image>
+            </div>
+
+            <ul className="flex justify-between items-center gap-3">
+                <li><Link href={"/profile"}>Profile</Link></li>
+                <li><Link href={"/login"}>Login</Link></li>
+                <li><Link href={"/signup"}>Sign Up</Link></li>
+            </ul>
             
-        </div>
+        </nav>
     );
 };
 

582ab93a0c77adef51d3414d6cbf32060f7d43c2
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-05-17 15:48:32 +0600

    Login feature added

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,70 +1,88 @@
 "use client";
 
+import { authClient } from "@/lib/auth-client";
 import { Card, Separator } from "@heroui/react";
 import {
-  Button,
-  Description,
-  FieldError,
-  Form,
-  Input,
-  Label,
-  TextField,
+    Button,
+    FieldError,
+    Form,
+    Input,
+    Label,
+    TextField,
 } from "@heroui/react";
+import { useRouter } from "next/navigation";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
 
 const LoginPage = () => {
-    const onSubmit = ()=>{
-
+    const router = useRouter();
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const userData = Object.fromEntries(formData.entries());
+        const { data, error } = await authClient.signIn.email({
+            email: userData.email,
+            password: userData.password,
+            rememberMe: true,
+        });
+        console.log(data);
+        if(data){
+            toast.success("Login Successful! 🎉");
+            router.push("/");
+        }
+        if(error){
+            toast.error(error.message);
+        }
     }
     return (
-    <div className="max-w-7xl mx-auto my-20">
-      <div className="text-center my-3">
-        <h1 className="text-2xl font-bold">Login</h1>
-        <p>Start your adventure with Wanderlust</p>
-      </div>
-      <Card className="border rounded-xl p-7">
-        <Form onSubmit={onSubmit} className="flex w-96 flex-col gap-4">
-          <TextField
-            isRequired
-            name="email"
-            type="email"
-          >
-            <Label>Email</Label>
-            <Input placeholder="john@example.com" />
-            <FieldError />
-          </TextField>
-          <TextField
-            isRequired
-            minLength={8}
-            name="password"
-            type="password"
-          >
-            <Label>Password</Label>
-            <Input placeholder="Enter your password" />
-          </TextField>
-          <div className="flex justify-center gap-2">
-            <Button className={" w-full bg-cyan-500"} type="submit">
-              Login
-            </Button>
-          </div>
-        </Form>
+        <div className="max-w-7xl mx-auto my-20">
+            <div className="text-center my-3">
+                <h1 className="text-2xl font-bold">Login</h1>
+                <p>Start your adventure with Wanderlust</p>
+            </div>
+            <Card className="border rounded-xl p-7">
+                <Form onSubmit={onSubmit} className="flex w-96 flex-col gap-4">
+                    <TextField
+                        isRequired
+                        name="email"
+                        type="email"
+                    >
+                        <Label>Email</Label>
+                        <Input placeholder="john@example.com" />
+                        <FieldError />
+                    </TextField>
+                    <TextField
+                        isRequired
+                        minLength={8}
+                        name="password"
+                        type="password"
+                    >
+                        <Label>Password</Label>
+                        <Input placeholder="Enter your password" />
+                    </TextField>
+                    <div className="flex justify-center gap-2">
+                        <Button className={" w-full bg-cyan-500"} type="submit">
+                            Login
+                        </Button>
+                    </div>
+                </Form>
 
-        <div className="flex justify-center items-center gap-3">
-          <Separator />
-          <div className="whitespace-nowrap"> Or sign up with </div>
-          <Separator />
+                <div className="flex justify-center items-center gap-3">
+                    <Separator />
+                    <div className="whitespace-nowrap"> Or sign up with </div>
+                    <Separator />
+                </div>
+                <div>
+                    <Button
+                        variant="outline"
+                        className={"w-full"}
+                    >
+                        <FcGoogle /> Sign in with Google
+                    </Button>
+                </div>
+            </Card>
         </div>
-        <div>
-          <Button
-            variant="outline"
-            className={"w-full"}
-          >
-            <FcGoogle /> Sign in with Google
-          </Button>
-        </div>
-      </Card>
-    </div>
-  );
+    );
 };
 
 export default LoginPage;
